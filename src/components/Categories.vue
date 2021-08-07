9126dbf788afefc525bdecfcd9e72dc58b57b94d
sasa-bonta <bonta2000@mail.ru>
2021-08-07 15:24:52 +0300

    subCategories background

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -8,49 +8,56 @@
       max-width="500"
     >
       <v-toolbar
-        color="teal"
+        color="blue darken-4"
         dark
       >
-        <v-toolbar-title>Topics</v-toolbar-title>
+        <v-toolbar-title>Categories</v-toolbar-title>
 
         <v-spacer />
       </v-toolbar>
-
-      <div
-        v-for="(item, i) in $store.getters['categories/getList']"
-        :key="'category-' + i"
+      <v-navigation-drawer
+        dark
+        src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        width="100%"
+        permanent
       >
-        <v-list
-          v-if="!item.parentLink"
+        <div
+          v-for="(item, i) in $store.getters['categories/getList']"
+          :key="'category-' + i"
         >
-          <v-list-group
-            v-model="item.active"
-            :prepend-icon="item.action"
-            no-action
+          <v-list
+            v-if="!item.parentLink"
           >
-            <template
-              v-slot:activator
+            <v-list-group
+              v-model="item.active"
+              :prepend-icon="item.action"
+              no-action
             >
-              <v-list-item-content>
-                <v-list-item-title v-text="item.name" />
-              </v-list-item-content>
-            </template>
-
-            <div
-              v-for="(subCategory, j) in $store.getters['categories/getList']"
-              :key="'subCategories-' + j"
-            >
-              <v-list-item
-                v-if="subCategory.parentLink === item.link"
+              <template
+                v-slot:activator
               >
                 <v-list-item-content>
-                  <v-list-item-title v-text="subCategory.name" />
+                  <v-list-item-title v-text="item.name" />
                 </v-list-item-content>
-              </v-list-item>
-            </div>
-          </v-list-group>
-        </v-list>
-      </div>
+              </template>
+
+              <div
+                v-for="(subCategory, j) in $store.getters['categories/getList']"  
+                :key="'subCategories-' + j"
+                class="subCategories"
+              >
+                <v-list-item
+                  v-if="subCategory.parentLink === item.link"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title v-text="subCategory.name" />
+                  </v-list-item-content>
+                </v-list-item>
+              </div>
+            </v-list-group>
+          </v-list>
+        </div>
+      </v-navigation-drawer>
     </v-card>
   </v-sheet>
 </template>
@@ -65,5 +72,14 @@ export default {
 </script>
 
 <style scoped>
+.v-application .primary--text {
+  color: #EC407A !important;
+  caret-color: #EC407A !important;
+  font-weight: bold;
+}
 
+.subCategories {
+  margin-left: 20px;
+  font-weight: normal;
+}
 </style>
