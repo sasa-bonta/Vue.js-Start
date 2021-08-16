0cfc769bc1ccb88572d7f483b55d4f5025d43af5
sasa-bonta <bonta2000@mail.ru>
2021-08-17 00:05:12 +0300

    error deleted

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,6 @@
 <template>
   <v-responsive max-width="260">
-<!--    <v-text-field-->
-<!--        :value="value"-->
-<!--        dense-->
-<!--        flat-->
-<!--        hide-details-->
-<!--        rounded-->
-<!--        solo-inverted-->
-<!--        v-on:keyup.enter="$emit('submitInput', $event.target.value)"-->
-<!--        v-on:keyup="$emit('input', $event.target.value)"-->
-<!--    />-->
     <v-autocomplete
-        :value="value"
         :items="$store.getters['suggestions/getSuggestions'].map(obj => obj.title)"
         dense
         flat
