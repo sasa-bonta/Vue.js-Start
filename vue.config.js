deb17a5766cedf58251dc58d397844c75b5c2f8b
sasa-bonta <bonta2000@mail.ru>
2021-08-26 03:01:05 +0300

    deleted proxies

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,6 @@
 module.exports = {
     devServer: {
         proxy: {
-            '^/api/categories': {
-                target: 'http://localhost:8003/categories',
-                pathRewrite: {
-                    '^/api/categories': '',
-                },
-                ws: true,
-                changeOrigin: true,
-            },
             '^/api/products': {
                 target: 'http://localhost:8003/page',
                 pathRewrite: {
@@ -17,22 +9,6 @@ module.exports = {
                 ws: true,
                 changeOrigin: true,
             },
-            '^/api/suggestions': {
-                target: 'https://999.md/suggestions',
-                pathRewrite: {
-                    '^/api/suggestions': ''
-                },
-                ws: true,
-                changeOrigin: true,
-            },
-            '^/api/item': {
-                target: 'http://localhost:8003/item',
-                pathRewrite: {
-                    '^/api/item': ''
-                },
-                ws: true,
-                changeOrigin: true,
-            }
         },
     },
     transpileDependencies: [
