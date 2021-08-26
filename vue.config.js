0d0c9abff9b7bc3ebda0e6113e4b9abc1158d75f
sasa-bonta <bonta2000@mail.ru>
2021-08-26 03:01:52 +0300

    proxies redone

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     devServer: {
         proxy: {
+            '^/api/categories': {
+                target: 'http://localhost:8003/categories',
+                pathRewrite: {
+                    '^/api/categories': '',
+                },
+                ws: true,
+                changeOrigin: true,
+            },
             '^/api/products': {
                 target: 'http://localhost:8003/page',
                 pathRewrite: {
@@ -9,6 +17,22 @@ module.exports = {
                 ws: true,
                 changeOrigin: true,
             },
+            '^/api/suggestions': {
+                target: 'https://999.md/suggestions',
+                pathRewrite: {
+                    '^/api/suggestions': ''
+                },
+                ws: true,
+                changeOrigin: true,
+            },
+            '^/api/item': {
+                target: 'http://localhost:8003/item',
+                pathRewrite: {
+                    '^/api/item': ''
+                },
+                ws: true,
+                changeOrigin: true,
+            }
         },
     },
     transpileDependencies: [
