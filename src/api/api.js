04c6d2c39eb7dbbe052b94dba9e23e168b29720c
sasa-bonta <bonta2000@mail.ru>
2021-09-08 17:47:22 +0300

    axios console

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,19 @@
 import axios from 'axios'
 
-export default axios.create({
+const myAxios = axios.create({
     baseURL: "http://999.appetit.md/api"
-})
+});
+
+myAxios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log("ok")
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    console.log("error")
+    return Promise.reject(error);
+});
+
+export default myAxios
